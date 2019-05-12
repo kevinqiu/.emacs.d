0633f287b0f5b66ce0b424f412309b5305de352e
Kevin Q <kevinqiu1@gmail.com>
2019-05-12 15:18:16 -0400

    different location on mac

--- config/lisp.el
+++ config/lisp.el
@@ -1,4 +1,6 @@
 ;; slime
 (use-package slime)
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(when (eq system-type 'darwin)
+  (setq inferior-lisp-program "/usr/local/bin/sbcl"))
 (setq slime-contribs '(slime-fancy))
